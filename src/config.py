24fa252720d832c3d52e780825e50ae1491242b7
ysreenu1523 <ysreenu.mba@gmail.com>
2026-06-01 11:52:56 +0530

    Comment out ORA_CONFIG in config.py
    Comment out ORA_CONFIG for Oracle database connection.

--- src/config.py
+++ src/config.py
@@ -4,9 +4,9 @@ CSV_FILE_PATH = 'data/sample_data.csv'
 #     'connection_string': 'ORACLE://scott:tiger@localhost:1523/mydatabase'
 # }
 
-ORA_CONFIG = {
-    'connection_string': 'ora://scott:tiger@localhost:1523/xe'
-}
+# ORA_CONFIG = {
+#     'connection_string': 'ora://scott:tiger@localhost:1523/xe'
+# }
 
 MYSQL_CONFIG ={
 'connection_string': 'mysql+pymysql://root:tiger@localhost/analytics'
