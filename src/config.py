5a00166860ea72efa28f1ee9c23ba1bebdab4e58
sreenu <ysreenu.mba@gmail.com>
2026-06-19 14:42:39 +0530

    test

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
-CSV_FILE_PATH = 'data/sample_data.csv'
+CSV_FILE_PATH = r'E:\python_project_demo\data\sample_data.csv'
 
 # DB_CONFIG = {
 #     'connection_string': 'ORACLE://scott:tiger@localhost:1523/mydatabase'
 # }
 
 ORA_CONFIG = {
-    'connection_string': 'ora://scott:tiger@localhost:1523/xe'
+    'connection_stringa': 'ora://scott:tiger@localhost:1523/xe'
 }
 
 MYSQL_CONFIG ={
